fd75e0d5c6950a691ffa960b63230cb351a95668
Vladimir Kuznetsov <vkuznetsov@vivid.money>
2022-08-25 11:57:17 +0300

    Easy: 383, 595, 607

---
+++ easy-difficulty/src/main/kotlin/com/leetcode/tasks/383/ransom/note/Solution.kt
@@ -0,0 +1,34 @@
+package com.leetcode.tasks.`383`.ransom.note
+
+/**
+ * 383. Ransom Note
+ * https://leetcode.com/problems/ransom-note/
+ */
+
+private object Solution {
+
+    fun canConstruct(ransomNote: String, magazine: String): Boolean {
+        val mapMagazine = mutableMapOf<Int, Int>()
+
+        for (c in magazine.chars()) {
+            mapMagazine[c] = mapMagazine.getOrDefault(c, 0) + 1
+        }
+
+        for (c in ransomNote.chars()) {
+            val charCount = mapMagazine[c]
+            if (charCount == null || charCount == 0) return false
+            else mapMagazine[c] = charCount - 1
+        }
+
+        return true
+    }
+
+}
+
+fun main() {
+
+    println(Solution.canConstruct("a", "b"))
+    println(Solution.canConstruct("aa", "ab"))
+    println(Solution.canConstruct("aa", "aab"))
+
+}
