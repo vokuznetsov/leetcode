101101f8c1a2e788a646faeb9d68064029401768
Vladimir Kuznetsov <vkuznetsov@vivid.money>
2022-10-29 23:29:28 +0500

    Easy: 1757

---
+++ easy-difficulty/src/main/kotlin/com/leetcode/tasks/1757/recyclable/and/low/fat/products/Solution.sql
@@ -0,0 +1,8 @@
+-- 1757. Recyclable and Low Fat Products
+-- https://leetcode.com/problems/recyclable-and-low-fat-products/
+
+Select
+       product_id
+From Products
+Where low_fats = 'Y' and recyclable = 'Y'
+
