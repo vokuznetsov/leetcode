ed0a3f3df931f84e9541d503b00acd3f1994999c
Vladimir Kuznetsov <vkuznetsov@vivid.money>
2022-11-01 11:45:15 +0500

    Easy: 290, 766

---
+++ easy-difficulty/src/main/kotlin/com/leetcode/tasks/290/word/pattern/Solution.kt
@@ -0,0 +1,47 @@
+package com.leetcode.tasks.`290`.word.pattern
+
+/**
+ * 283. Move Zeroes
+ * https://leetcode.com/problems/move-zeroes/
+ */
+
+private object Solution {
+
+    fun wordPattern(pattern: String, s: String): Boolean {
+        val arr = MutableList(26) { "" }
+        var wordStartIndex = 0
+
+        for (c in pattern) {
+            if (wordStartIndex >= s.length) return false
+
+            var wordEndIndex = wordStartIndex
+            while (wordEndIndex != s.length && s[wordEndIndex] != ' ') {
+                wordEndIndex++
+            }
+
+            val word = s.substring(wordStartIndex, wordEndIndex)
+            wordStartIndex = if (wordEndIndex == s.length) wordEndIndex else wordEndIndex + 1
+
+            if (arr[c - 'a'].isEmpty()) {
+                if (arr.contains(word))
+                    return false
+                else arr[c - 'a'] = word
+            } else if (arr[c - 'a'] != word) return false
+        }
+
+        return wordStartIndex == s.length
+    }
+
+}
+
+fun main() {
+
+
+    println(Solution.wordPattern("abba", "dog cat cat dog"))
+    println(Solution.wordPattern("abba", "dog dog dog dog"))
+    println(Solution.wordPattern("abba", "dog cat cat fish"))
+    println(Solution.wordPattern("aaaa", "dog cat cat dog"))
+    println(Solution.wordPattern("aaa", "aa aa aa aa"))
+    println(Solution.wordPattern("aaaaa", "aa aa"))
+
+}
---
+++ easy-difficulty/src/main/kotlin/com/leetcode/tasks/766/toeplitz/matrix/Solution.kt
@@ -0,0 +1,77 @@
+package com.leetcode.tasks.`766`.toeplitz.matrix
+
+/**
+ * 766. Toeplitz Matrix
+ * https://leetcode.com/problems/toeplitz-matrix/
+ */
+
+private object Solution {
+
+    fun isToeplitzMatrix(matrix: Array<IntArray>): Boolean {
+        val m = matrix.size
+        val n = matrix[0].size
+        var x = 0
+        var y = 0
+
+
+        // iterate by 0 row
+        while (y < matrix[0].size) {
+            var next = 0
+            var elem = matrix[x][y]
+            while (x + next < m && y + next < n) {
+                if (elem != matrix[x + next][y + next]) return false
+                next++
+            }
+            y++
+        }
+        x++
+        y = 0
+
+        while (x < m) {
+            var next = 0
+            var elem = matrix[x][y]
+            while (x + next < m && y + next < n) {
+                if (elem != matrix[x + next][y + next]) return false
+                next++
+            }
+            x++
+            y = 0
+        }
+
+        return true
+    }
+
+    // the same as previous but more readable solution
+    fun isToeplitzMatrix1(matrix: Array<IntArray>): Boolean {
+        for (x in 1 until matrix.size) {
+            for (y in 1 until matrix[x].size) {
+                if (matrix[x - 1][y - 1] != matrix[x][y]) return false
+            }
+        }
+
+        return true
+    }
+
+}
+
+fun main() {
+
+    println(
+        Solution.isToeplitzMatrix1(
+            arrayOf(
+                intArrayOf(1, 2, 3, 4),
+                intArrayOf(5, 1, 2, 3),
+                intArrayOf(9, 5, 1, 2)
+            )
+        )
+    )
+
+    println(
+        Solution.isToeplitzMatrix1(
+            arrayOf(
+                intArrayOf(1, 2),
+                intArrayOf(2, 2),
+            )
+        )
+    )
+}
