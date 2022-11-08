ede370bf29ace717fae099e6c15a21cea3e94cdb
Vladimir Kuznetsov <vladimir.kuznetsov@almatech.dev>
2022-11-08 15:53:27 +0500

    Easy: 1544

---
+++ easy-difficulty/src/main/kotlin/com/leetcode/tasks/algorithms/1544/make/the/string/Solution.kt
@@ -0,0 +1,34 @@
+package com.leetcode.tasks.algorithms.`1544`.make.the.string
+
+/**
+ * 1544. Make The String Great
+ * https://leetcode.com/problems/make-the-string-great/
+ */
+
+private object Solution {
+
+    fun makeGood(s: String): String {
+        val stack = ArrayDeque<Char>()
+
+        for (c in s) {
+            if (stack.isNotEmpty() && Math.abs(c - stack.last()) == 32) stack.removeLast()
+            else stack.addLast(c)
+        }
+
+        return stack.joinToString("")
+    }
+}
+
+fun main() {
+
+    println(Solution.makeGood("leEeetcode") == "leetcode")
+    println(Solution.makeGood("") == "")
+    println(Solution.makeGood("abBAcC") == "")
+    println(Solution.makeGood("abBAacC") == "a")
+    println(Solution.makeGood("abBAacCA") == "")
+    println(Solution.makeGood("RLlr") == "")
+    println(Solution.makeGood("RLlr") == "")
+    println(Solution.makeGood("Rr") == "")
+    println(Solution.makeGood("RrL") == "L")
+
+}
