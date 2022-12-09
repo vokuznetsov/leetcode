8f7f057dbc1ab0ce2644fa66a579773b3b6a3249
Vladimir Kuznetsov <vladimir.kuznetsov@almatech.dev>
2022-12-09 13:13:55 +0500

    utility class

--- utility/src/main/kotlin/com/leetcode/utility/UtilClass.kt
+++ utility/src/main/kotlin/com/leetcode/utility/UtilClass.kt
@@ -19,19 +19,19 @@ data class ListNode(var `val`: Int, var next: ListNode? = null) {
 data class TreeNode(var `val`: Int, var left: TreeNode? = null, var right: TreeNode? = null) {
 
     companion object {
-        fun create(intArray: Array<Int?>): TreeNode? {
-            if (intArray.isEmpty()) return null
-            val headTree = TreeNode(intArray[0]!!)
+        fun create(array: Array<Int?>): TreeNode? {
+            if (array.isEmpty()) return null
+            val headTree = TreeNode(array[0]!!)
 
             val stack = ArrayDeque<TreeNode>()
             stack.addLast(headTree)
 
             var index = 1
 
-            while (index != intArray.size) {
+            while (index != array.size) {
                 val tree = if (index % 2 == 0) stack.removeFirst() else stack.first()
 
-                val node = intArray[index]?.let {
+                val node = array[index]?.let {
                     val n = TreeNode(it)
                     stack.addLast(n)
                     n
