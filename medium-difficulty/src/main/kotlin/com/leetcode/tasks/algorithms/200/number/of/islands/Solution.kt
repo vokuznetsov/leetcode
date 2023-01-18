d0b409d4ef315d38354a977f5f15d1952ee842ea
Vladimir Kuznetsov <vladimir.kuznetsov@almatech.dev>
2023-01-18 19:18:26 +0500

    Medium: 200

---
+++ medium-difficulty/src/main/kotlin/com/leetcode/tasks/algorithms/200/number/of/islands/Solution.kt
@@ -0,0 +1,116 @@
+package com.leetcode.tasks.algorithms.`200`.number.of.islands
+
+/**
+ * 200. Number of Islands
+ * https://leetcode.com/problems/number-of-islands/
+ */
+private object Solution {
+
+    private var count = 0
+    private val visitedElement = mutableSetOf<Pair<Int, Int>>()
+
+    fun numIslands(grid: Array<CharArray>): Int {
+        count = 0
+        visitedElement.clear()
+        val m = grid.lastIndex
+        val n = grid[0].lastIndex
+
+        for (x in 0..m) {
+            for (y in 0..n) {
+                if (grid[x][y] == '0') visitedElement.add(x to y)
+                else if (!visitedElement.contains(x to y) && isIsland(x, y, m, n, grid)) {
+                    count++
+                }
+            }
+        }
+
+        return count
+    }
+
+    private fun isIsland(x: Int, y: Int, m: Int, n: Int, grid: Array<CharArray>): Boolean {
+        if (x > m || y > n || x < 0 || y < 0 || grid[x][y] == '0' || visitedElement.contains(x to y)) return true
+        else {
+            visitedElement.add(x to y)
+
+            return isIsland(x - 1, y, m, n, grid)
+                    && isIsland(x + 1, y, m, n, grid)
+                    && isIsland(x, y - 1, m, n, grid)
+                    && isIsland(x, y + 1, m, n, grid)
+        }
+    }
+}
+
+fun main() {
+
+    println(
+        Solution.numIslands(
+            arrayOf(
+                charArrayOf('1', '0', '1'),
+                charArrayOf('1', '1', '1')
+            )
+        ) == 1
+    )
+
+
+    println(
+        Solution.numIslands(
+            arrayOf(
+                charArrayOf('1', '0', '1'),
+                charArrayOf('1', '1', '0')
+            )
+        ) == 2
+    )
+
+    println(
+        Solution.numIslands(
+            arrayOf(
+                charArrayOf('1', '1', '1', '1', '0'),
+                charArrayOf('1', '1', '0', '1', '0'),
+                charArrayOf('1', '1', '0', '0', '0'),
+                charArrayOf('0', '0', '0', '0', '0')
+            )
+        ) == 1
+    )
+
+    println(
+        Solution.numIslands(
+            arrayOf(
+                charArrayOf('1', '0'),
+                charArrayOf('0', '1')
+            )
+        ) == 2
+    )
+
+    println(
+        Solution.numIslands(
+            arrayOf(
+                charArrayOf('1', '1', '0', '0'),
+                charArrayOf('1', '1', '0', '0'),
+                charArrayOf('0', '0', '1', '0'),
+                charArrayOf('0', '0', '0', '1')
+            )
+        ) == 3
+    )
+
+    println(
+        Solution.numIslands(
+            arrayOf(
+
+                charArrayOf('1', '0', '0'),
+                charArrayOf('0', '1', '1')
+            )
+        ) == 2
+    )
+
+    println(
+        Solution.numIslands(
+            arrayOf(
+                charArrayOf('1', '1', '0', '0', '0'),
+                charArrayOf('1', '1', '0', '0', '0'),
+                charArrayOf('0', '0', '1', '0', '0'),
+                charArrayOf('0', '0', '0', '1', '1')
+            )
+        ) == 3
+    )
+
+}
