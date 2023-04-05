761c2d95279bd4fd3684754bda8aeab5392c337a
Vladimir Kuznetsov <vladimir.kuznetsov@almatech.dev>
2023-04-05 22:28:04 +0500

    Easy: 303

--- easy-difficulty/src/main/kotlin/com/leetcode/tasks/algorithms/303/range/sum/query/immutable/Solution.kt
+++ easy-difficulty/src/main/kotlin/com/leetcode/tasks/algorithms/303/range/sum/query/immutable/Solution.kt
@@ -13,24 +13,17 @@ package com.leetcode.tasks.algorithms.`303`.range.sum.query.immutable
 
 class NumArray(val nums: IntArray) {
 
-    val result: IntArray = IntArray(nums.size)
+    val cache = IntArray(nums.size)
 
     init {
-        result[0] = nums[0]
-        for (i in 1 until nums.size)
-            result[i] = nums[i] + result[i - 1]
+        cache[0] = nums[0]
+        for (i in 1..nums.lastIndex) {
+            cache[i] = cache[i - 1] + nums[i]
+        }
     }
 
     fun sumRange(left: Int, right: Int): Int {
-        var sum = 0
-        for (i in left..right)
-            sum += nums[i]
-        return sum
-    }
-
-    // O(1) memory and O(n) space
-    fun sumRange1(left: Int, right: Int): Int {
-        return if (left == 0) result[right] else result[right] - result[left - 1]
+        return cache[right] - cache[left] + nums[left]
     }
 
 }
@@ -38,8 +31,8 @@ class NumArray(val nums: IntArray) {
 fun main() {
 
     val numArray = NumArray(intArrayOf(-2, 0, 3, -5, 2, -1))
-    println(numArray.sumRange1(0, 2) == 1)
-    println(numArray.sumRange1(2, 5) == -1)
-    println(numArray.sumRange1(0, 5) == -3)
+    println(numArray.sumRange(0, 2) == 1)
+    println(numArray.sumRange(2, 5) == -1)
+    println(numArray.sumRange(0, 5) == -3)
 
 }
